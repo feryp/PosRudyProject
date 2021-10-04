5dd7eaccab82ae71c776fa7b02eeb8dbbfb2dab1
danni <yogadanni0@gmail.com>
2021-10-04 21:47:29 +0700

    add dummy login function

--- app/src/main/java/com/example/posrudyproject/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/posrudyproject/ui/login/LoginActivity.java
@@ -2,15 +2,57 @@ package com.example.posrudyproject.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.posrudyproject.R;
+import com.example.posrudyproject.ui.main.MainActivity;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText etEmail, etPassword;
+    MaterialButton btnLogin;
+    TextInputLayout etLayout;
+
+    String USER = "admin";
+    String PASSWORD = "admin";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initComponent();
+
+        btnLogin.setOnClickListener(view -> {
+            validation();
+        });
+
     }
+
+    public void initComponent(){
+        // init
+        etEmail = findViewById(R.id.et_nama_pengguna_login);
+        etPassword = findViewById(R.id.et_kata_sandi_login);
+        etLayout = findViewById(R.id.layout_et_nama_pengguna);
+        btnLogin = findViewById(R.id.btn_masuk);
+
+    }
+
+    public void validation(){
+        if(Objects.requireNonNull(etEmail.getText()).toString().equals(USER) &&
+                Objects.requireNonNull(etPassword.getText()).toString().equals(PASSWORD)){
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+
 }
