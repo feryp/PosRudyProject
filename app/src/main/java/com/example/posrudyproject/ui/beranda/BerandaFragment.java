137ccb33c0ea05d7a06134840cff3e02835cda63
Fery Bithealth <fery.pratama20@gmail.com>
2021-10-13 21:19:42 +0700

    update main : - create function selected bottom navigation - add fragment_beranda - add fragment_rekap_kas - add fragment_keranjang - add fragment_akun - edit menu_botnav.xml

---
+++ app/src/main/java/com/example/posrudyproject/ui/beranda/BerandaFragment.java
@@ -0,0 +1,21 @@
+package com.example.posrudyproject.ui.beranda;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.posrudyproject.R;
+
+public class BerandaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_beranda, container, false);
+    }
+}
