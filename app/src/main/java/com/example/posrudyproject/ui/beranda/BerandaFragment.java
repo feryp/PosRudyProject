099f0d939d6c5506d6dfb90222efef26f5a5489c
Fery Bithealth <fery.pratama20@gmail.com>
2021-10-14 22:14:33 +0700

    update UI Beranda : - add new icon - add dummy image - add adapter ImageSliderAdapter - add viewholder ImageSliderViewHolder - add model ImageSliderItem - add layout include menu_kategori.xml

--- app/src/main/java/com/example/posrudyproject/ui/beranda/BerandaFragment.java
+++ app/src/main/java/com/example/posrudyproject/ui/beranda/BerandaFragment.java
@@ -2,20 +2,92 @@ package com.example.posrudyproject.ui.beranda;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.posrudyproject.R;
+import com.example.posrudyproject.ui.beranda.adapter.ImageSliderAdapter;
+import com.example.posrudyproject.ui.beranda.model.ImageSliderItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BerandaFragment extends Fragment {
+
+    private ViewPager2 vp2;
+    private ImageSliderAdapter imageSliderAdapter;
+    private final Handler sliderHandler = new Handler();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beranda, container, false);
+        View v = inflater.inflate(R.layout.fragment_beranda, container, false);
+
+        //INIT VIEW
+        vp2 = v.findViewById(R.id.vp_image_slider);
+
+        List<ImageSliderItem> sliderItems = new ArrayList<>();
+        sliderItems.add(new ImageSliderItem(R.drawable.im_slide_1));
+        sliderItems.add(new ImageSliderItem(R.drawable.im_slide_2));
+        sliderItems.add(new ImageSliderItem(R.drawable.im_slide_3));
+
+        vp2.setAdapter(new ImageSliderAdapter(sliderItems, vp2));
+        vp2.setCurrentItem(2, true); //Set currentItem index : 2
+        vp2.setClipToPadding(false);
+        vp2.setClipChildren(false);
+        vp2.setOffscreenPageLimit(3);
+        vp2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        vp2.setPageTransformer(compositePageTransformer);
+
+        vp2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable, 3000); //slide duration 3 seconds
+            }
+        });
+
+        return v;
+    }
+
+    private final Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            vp2.setCurrentItem(vp2.getCurrentItem() + 1);
+        }
+    };
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sliderHandler.postDelayed(sliderRunnable, 3000);
     }
 }
