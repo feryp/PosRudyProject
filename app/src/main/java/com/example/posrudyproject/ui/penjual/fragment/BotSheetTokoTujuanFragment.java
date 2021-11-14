489a5669482554924dedf24a538b20ce2c9696f4
Fery Bithealth <fery.pratama20@gmail.com>
2021-11-14 17:23:39 +0700

    update UI Penjual : - edit BotSheetTokoTujuanFragment

--- app/src/main/java/com/example/posrudyproject/ui/penjual/fragment/BotSheetTokoTujuanFragment.java
+++ app/src/main/java/com/example/posrudyproject/ui/penjual/fragment/BotSheetTokoTujuanFragment.java
@@ -90,13 +90,14 @@ public class BotSheetTokoTujuanFragment extends BottomSheetDialogFragment implem
     @SuppressLint({"SetTextI18n","UseCompatLoadingForDrawables"})
     private void openDialog() {
         final AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
-        View mView = getLayoutInflater().inflate(R.layout.dialog_alert_message, null);
+//        View mView = getLayoutInflater().inflate(R.layout.dialog_alert_message, null);
+        View mView = View.inflate(getActivity(), R.layout.dialog_alert_message,null);
 
         //init view
-        AppCompatTextView titleDialog = mView.findViewById(R.id.txt_title_dialog);
-        AppCompatTextView pesanDialog = mView.findViewById(R.id.txt_pesan_dialog);
-        MaterialButton btnOke = mView.findViewById(R.id.btn_oke_dialog);
-        MaterialButton btnCancel = mView.findViewById(R.id.btn_cancel_dialog);
+        AppCompatTextView titleDialog = (AppCompatTextView) mView.findViewById(R.id.txt_title_dialog);
+        AppCompatTextView pesanDialog = (AppCompatTextView) mView.findViewById(R.id.txt_pesan_dialog);
+        MaterialButton btnOke = (MaterialButton) mView.findViewById(R.id.btn_oke_dialog);
+        MaterialButton btnCancel = (MaterialButton) mView.findViewById(R.id.btn_cancel_dialog);
 
         alert.setView(mView);
 
@@ -111,7 +112,6 @@ public class BotSheetTokoTujuanFragment extends BottomSheetDialogFragment implem
         btnOke.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(getActivity(), "Oke", Toast.LENGTH_SHORT).show();
                 alertDialog.dismiss();
             }
         });
