46581e48a37f887efc6c4f877824df1d3d5a7e8e
Fery Bithealth <fery.pratama20@gmail.com>
2021-12-14 19:24:27 +0700

    update edit UI Penyimpanan: - edit KategoriPenyimpananViewHolder - edit KategoriPenyimpananAdapter - edit activity_barang_keluar.xml - edit activity_barang_masuk.xml - edit activity_barang_pindah.xml
    - add bg_border_rounded_davy_grey.xml
    - add bg_rounded_half_green.xml
    - add ic_arrow_down_gray.xml
    - add item_barang_masuk.xml

--- app/src/main/java/com/example/posrudyproject/ui/penyimpanan/viewholder/KategoriPenyimpananViewHolder.java
+++ app/src/main/java/com/example/posrudyproject/ui/penyimpanan/viewholder/KategoriPenyimpananViewHolder.java
@@ -8,15 +8,18 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.posrudyproject.R;
+import com.google.android.material.card.MaterialCardView;
 
 public class KategoriPenyimpananViewHolder extends RecyclerView.ViewHolder {
 
+    public MaterialCardView container;
     public AppCompatTextView jumlahBarang;
     public AppCompatTextView kategoriBarang;
 
     public KategoriPenyimpananViewHolder(@NonNull View itemView) {
         super(itemView);
 
+        container = itemView.findViewById(R.id.card_kategori_penyimpanan);
         jumlahBarang = itemView.findViewById(R.id.tv_jumlah_barang_kategori_penyimpanan);
         kategoriBarang = itemView.findViewById(R.id.tv_kategori_barang_penyimpanan);
     }
