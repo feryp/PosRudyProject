93fdb2a18fd335a6f7510a8ee662b18d5f53bd58
Fery Bithealth <fery.pratama20@gmail.com>
2021-12-14 13:46:09 +0700

    update edit UI Penyimpanan: - add BarangMasukActivity - add BarangKeluarActivity - add BarangPindahActivity

---
+++ app/src/main/java/com/example/posrudyproject/ui/barang/barangKeluar/BarangKeluarActivity.java
@@ -0,0 +1,16 @@
+package com.example.posrudyproject.ui.barang.barangKeluar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.posrudyproject.R;
+
+public class BarangKeluarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barang_keluar);
+    }
+}
---
+++ app/src/main/java/com/example/posrudyproject/ui/barang/barangPindah/BarangPindahActivity.java
@@ -0,0 +1,16 @@
+package com.example.posrudyproject.ui.barang.barangPindah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.posrudyproject.R;
+
+public class BarangPindahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barang_pindah);
+    }
+}
