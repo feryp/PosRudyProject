fab7a2eb18c30cda78430e06c807fb8925802fe7
Fery Bithealth <fery.pratama20@gmail.com>
2022-01-03 16:54:50 +0700

    update UI Akun : - add DetailTransaksiSelesaiActivity - add activity_detail_transaksi_selesai.xml
    - edit PengaturanActivity
    - edit RiwayatSelesaiFragment
    - edit activity_detail_riwayat.xml
    - edit activity_keranjang.xml
    - edit activity_pengaturan.xml
    - edit activity_transaksi_sukses.xml
    - edit fragment_riwayat_selesai.xml
    
    - edit strings.xml
    - edit themes.xml
    - edit AndroidManifest.xml

--- app/src/main/java/com/example/posrudyproject/ui/akun/activity/PengaturanActivity.java
+++ app/src/main/java/com/example/posrudyproject/ui/akun/activity/PengaturanActivity.java
@@ -1,16 +1,102 @@
 package com.example.posrudyproject.ui.akun.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.example.posrudyproject.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
-public class PengaturanActivity extends AppCompatActivity {
+public class PengaturanActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+    MaterialToolbar mToolbar;
+    SwitchCompat pushNotif, emailNotif;
+    AppCompatTextView printerSetting;
+    SharedPreferences sharedPreferences;
+    String PACKAGE_NAME = "com.example.posrudyproject";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengaturan);
+
+        //INIT VIEW
+        initComponent();
+
+        initToolbar();
+
+        //SET LISTENER
+        printerSetting.setOnClickListener(this);
+
+        if (pushNotif != null){
+            pushNotif.setOnCheckedChangeListener(this);
+        }
+
+        if (emailNotif != null){
+            emailNotif.setOnCheckedChangeListener(this);
+        }
+    }
+
+    private void initToolbar() {
+        mToolbar.setNavigationOnClickListener(view -> finish());
+    }
+
+    private void initComponent() {
+        mToolbar = findViewById(R.id.toolbar_pengaturan);
+        pushNotif = findViewById(R.id.toogle_switch_push_notif);
+        emailNotif = findViewById(R.id.toogle_switch_email);
+        printerSetting = findViewById(R.id.tv_setting_printer);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        switch (compoundButton.getId()){
+            case R.id.toogle_switch_push_notif:
+                Toast.makeText(this, "Notification Switch " + (isChecked ? "On" : "Off"),Toast.LENGTH_SHORT).show();
+                if (isChecked) {
+                    Toast.makeText(this, "Notifikasi Nyala", Toast.LENGTH_SHORT).show();
+                    pushNotif.setChecked(true);
+                    saveNotifSetting(true);
+                } else {
+                    Toast.makeText(this, "Notifikasi Mati", Toast.LENGTH_SHORT).show();
+                    pushNotif.setChecked(false);
+                    saveNotifSetting(false);
+                }
+                break;
+            case R.id.toogle_switch_email:
+                Toast.makeText(this, "Notification Switch " + (isChecked ? "On" : "Off"),Toast.LENGTH_SHORT).show();
+                if (isChecked) {
+                    Toast.makeText(this, "Notifikasi Nyala", Toast.LENGTH_SHORT).show();
+                    emailNotif.setChecked(true);
+                    saveNotifSetting(true);
+                } else {
+                    Toast.makeText(this, "Notifikasi Mati", Toast.LENGTH_SHORT).show();
+                    emailNotif.setChecked(false);
+                    saveNotifSetting(false);
+                }
+                break;
+        }
+    }
+
+    private void saveNotifSetting(boolean notification) {
+        sharedPreferences = getApplicationContext().getSharedPreferences(PACKAGE_NAME + "SETTING_NOTIF", Context.MODE_PRIVATE);
+        sharedPreferences.getBoolean("notif", false);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("notif", notification);
+        editor.apply();
     }
 }
